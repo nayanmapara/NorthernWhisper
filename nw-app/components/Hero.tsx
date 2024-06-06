d8dc9353005594f089c827047bbee29d6d2b5f7c
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2024-06-05 23:13:36 -0400

    Updated Headers and Created Required Files and Setup Base Code

---
+++ nw-app/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="space-y-1">
+        <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">
+          Join the waitlist for my
+        </h2>
+        <h1 className="z-10 text-4xl font-bold text-center text-transparent duration-1000 bg-white bg-gradient-to-r from-cyan-50 to-cyan-500 animate-fade-in-3 cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text">
+          Newsletter
+        </h1>
+      </div>
+  )
+}
+
+export default Hero
---
+++ nw-app/components/NewsletterForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewsletterForm() {
+  return (
+    <div>NewsletterForm</div>
+  )
+}
+
+export default NewsletterForm
