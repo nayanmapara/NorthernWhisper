448ed3f28995e79a96b6808b5186b2c12ec7a559
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2024-06-07 00:11:54 -0400

    Added the Input box with design & animations

--- nw-app/app/page.tsx
+++ nw-app/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import Hero from "../components/Hero";
-import NewsletterForm from "../components/NewsletterForm";
+import Hero from "@/components/Hero";
+import NewsletterForm from "@/components/NewsletterForm";
 
 export default function Home() {
   return (
     <main className="bg-[#03040B] flex flex-col items-center justify-center p-10 min-h-screen">
       {/* <Header/> */}
       <Hero/>
-      {/* <NewsletterForm/> */}
+      <NewsletterForm/>
       {/* <Socials/> */}
       {/* <Footer/> */}
     </main>
--- nw-app/components/NewsletterForm.tsx
+++ nw-app/components/NewsletterForm.tsx
@@ -1,8 +1,80 @@
-import React from 'react'
+"use client";
+
+import React, { useState, useRef, FormEvent } from 'react';
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
+import { gsap } from 'gsap';
+import { getPlaneKeyframes } from '@/lib/getPlaneKeyframes';
+import { getTrailsKeyframes } from '@/lib/getTrailsKeyframes';
 
 function NewsletterForm() {
+  const [input, setInput] = useState("");
+  const [active, setActive] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const {to, fromTo, set} = gsap;
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const email = input;
+    const button = buttonRef.current
+
+    if (!email || !button) return;
+
+    if (!active) {
+      setActive(true);
+       
+      // Animation for plane
+      to(button, {
+        keyframes: getPlaneKeyframes(set, fromTo, button, setActive, setInput),
+      })
+
+      // Animation for trails
+      to(button, {
+        keyframes: getTrailsKeyframes(button),
+      })
+    }
+  }
+
   return (
-    <div>NewsletterForm</div>
+    <div className="flex flex-col space-y-8 md:w-[400px]">
+      <form className="newsletter-form mt-10 animate-fade-in-3" onSubmit={handleSubmit}>
+        <div className="group flex items-center gap-x-4 py-1 pl-4 pr-1 rounded-[9px] bg-[#090D11] hover:bg-[#14171b] shadow-outline-gray hover:shadow-transparent focus-within:bg-[#15141B] focus-within:!shadow-outline-gray-focus transition-all duration-300">
+          <EnvelopeIcon className="hidden sm:inline w-6 h-6 text-[#4B4C52] group-focus-within:text-white group-hover:text-white transition-colors duration-300" />
+          <input 
+            value={input} 
+            onChange={e => setInput(e.target.value)} 
+            type='email' 
+            placeholder='Email Address' 
+            required
+            className="flex-1 text-white text-sm sm:text-base outline-none placeholder-[#4B4C52] group-focus-within:placeholder-white bg-transparent placeholder:transition-colors placeholder:duration-300"
+          />
+          <button
+            ref={buttonRef}
+            className={`${
+              active && "active"
+            } disabled:!bg-[#6D86CB] disabled:grayscale-[65%] disabled:opacity-50 disabled:cursor-not-allowed text-sm md:text-base`}
+            disabled={!input}
+            type="submit"
+          >
+            <span className="default">Subscribe</span>
+            <span className="success">
+              <svg viewBox="0 0 16 16">
+                <polyline points="3.75 9 7 12 13 5"></polyline>
+              </svg>
+              Done
+            </span>
+            <svg className="trails" viewBox="0 0 33 64">
+              <path d="M26,4 C28,13.3333333 29,22.6666667 29,32 C29,41.3333333 28,50.6666667 26,60"></path>
+              <path d="M6,4 C8,13.3333333 9,22.6666667 9,32 C9,41.3333333 8,50.6666667 6,60"></path>
+            </svg>
+            <div className="plane">
+              <div className="left"></div>
+              <div className="right"></div>
+            </div>
+          </button>
+        </div>    
+      </form>
+    </div>
   )
 }
 
