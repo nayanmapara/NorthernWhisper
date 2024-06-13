a442d9d322c150cbb0004a7af1fbf9af1bc13048
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2024-06-13 18:17:42 -0400

    Updated favicon

--- nw-app/app/layout.tsx
+++ nw-app/app/layout.tsx
@@ -17,7 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+
+      {/* Font Style for logo */}
       <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
